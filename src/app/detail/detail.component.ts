fe9bf967a8dad8e62f61e714e99b49c67ed9364c
AyanBhadury <ayon.bony@gmail.com>
2021-08-23 18:59:19 +0530

    v1.0.0

--- src/app/detail/detail.component.ts
+++ src/app/detail/detail.component.ts
@@ -6,8 +6,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./detail.component.scss']
 })
 export class DetailComponent implements OnInit {
-
-  constructor() { }
+  dataSource: Object;
+  constructor() { 
+    
+    this.dataSource = {
+      chart: {
+        caption: 'Countries With Most Oil Reserves [2017-18]',
+        subCaption: 'In MMbbl = One Million barrels',
+        xAxisName: 'Country',
+        yAxisName: 'Reserves (MMbbl)',
+        numberSuffix: 'K',
+        theme: 'fusion',
+        bgAlpha:0
+      },
+      data: [
+        { label: 'Venezuela', value: '290' },
+        { label: 'Saudi', value: '260' },
+        { label: 'Canada', value: '180' },
+        { label: 'Iran', value: '140' },
+        { label: 'Russia', value: '115' },
+        { label: 'UAE', value: '100' },
+        { label: 'US', value: '30' },
+        { label: 'China', value: '30' }
+      ]
+    };
+  }
 
   ngOnInit(): void {
     console.log('DetailComponent INIT');
--- src/app/detail/detail.module.ts
+++ src/app/detail/detail.module.ts
@@ -5,9 +5,19 @@ import { DetailRoutingModule } from './detail-routing.module';
 
 import { DetailComponent } from './detail.component';
 import { SharedModule } from '../shared/shared.module';
+import { FusionChartsModule } from 'angular-fusioncharts';
+
+// Import FusionCharts library and chart modules
+import * as FusionCharts from 'fusioncharts';
+import * as Charts from 'fusioncharts/fusioncharts.charts';
+import * as FusionTheme from 'fusioncharts/themes/fusioncharts.theme.fusion';
+
+// Pass the fusioncharts library and chart modules
+FusionChartsModule.fcRoot(FusionCharts, Charts, FusionTheme);
+
 
 @NgModule({
   declarations: [DetailComponent],
-  imports: [CommonModule, SharedModule, DetailRoutingModule]
+  imports: [CommonModule, SharedModule, DetailRoutingModule,FusionChartsModule]
 })
 export class DetailModule {}
